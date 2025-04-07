23ca3e12498fc7a4c84e3101fb8767517723cc28
imdadfarooq <imdad.kaul@lelafe.com>
2025-04-07 23:26:42 +0530

    landing page complete

--- gutenBerg/src/app/components/landing-page/landing-page.component.ts
+++ gutenBerg/src/app/components/landing-page/landing-page.component.ts
@@ -1,12 +1,26 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-landing-page',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './landing-page.component.html',
   styleUrl: './landing-page.component.scss'
 })
 export class LandingPageComponent {
+  cardData: { icon: string; title: string; nextIcon: string; }[] = [];
+  cardData2: { icon: string; title: string; nextIcon: string; }[] = [];
+  ngOnInit() {
+    this.cardData = [
+      { icon: '../../../assets/images/Fiction.svg', title: 'FICTION', nextIcon: '../../../assets/images/Next.svg' },
+      { icon: '../../../assets/images/Drama.svg', title: 'DRAMA', nextIcon: '../../../assets/images/Next.svg' },
+      { icon: '../../../assets/images/Humour.svg', title: 'HUMOUR', nextIcon: '../../../assets/images/Next.svg' },
+      { icon: '../../../assets/images/Politics.svg', title: 'POLITICS', nextIcon: '../../../assets/images/Next.svg' },
 
+    ]
+    this.cardData2 = [{ icon: '../../../assets/images/Philosophy.svg', title: 'PHILOSOPHY', nextIcon: '../../../assets/images/Next.svg' },
+    { icon: '../../../assets/images/History.svg', title: 'HISTORY', nextIcon: '../../../assets/images/Next.svg' },
+    { icon: '../../../assets/images/Adventure.svg', title: 'ADVENTURE', nextIcon: '../../../assets/images/Next.svg' }]
+  }
 }
