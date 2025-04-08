ce117b89a9c4ad28c112baf1d6a8b80c1213b2c7
imdadfarooq <imdad.kaul@lelafe.com>
2025-04-08 11:12:08 +0530

    link fix

--- src/app/components/landing-page/landing-page.component.ts
+++ src/app/components/landing-page/landing-page.component.ts
@@ -15,13 +15,13 @@ export class LandingPageComponent {
   ngOnInit() {
     this.cardData = [
       { icon: '../../../assets/images/Fiction.svg', title: 'FICTION', nextIcon: '../../../assets/images/Next.svg', url: '/fiction' },
-      { icon: '../../../assets/images/Drama.svg', title: 'DRAMA', nextIcon: '../../../assets/images/Next.svg', url: '/fiction' },
-      { icon: '../../../assets/images/Humour.svg', title: 'HUMOUR', nextIcon: '../../../assets/images/Next.svg', url: '/fiction' },
-      { icon: '../../../assets/images/Politics.svg', title: 'POLITICS', nextIcon: '../../../assets/images/Next.svg', url: '/fiction' },
+      { icon: '../../../assets/images/Drama.svg', title: 'DRAMA', nextIcon: '../../../assets/images/Next.svg', url: '' },
+      { icon: '../../../assets/images/Humour.svg', title: 'HUMOUR', nextIcon: '../../../assets/images/Next.svg', url: '' },
+      { icon: '../../../assets/images/Politics.svg', title: 'POLITICS', nextIcon: '../../../assets/images/Next.svg', url: '' },
 
     ]
-    this.cardData2 = [{ icon: '../../../assets/images/Philosophy.svg', title: 'PHILOSOPHY', nextIcon: '../../../assets/images/Next.svg', url: '/fiction' },
-    { icon: '../../../assets/images/History.svg', title: 'HISTORY', nextIcon: '../../../assets/images/Next.svg', url: '/fiction' },
-    { icon: '../../../assets/images/Adventure.svg', title: 'ADVENTURE', nextIcon: '../../../assets/images/Next.svg', url: '/fiction' }]
+    this.cardData2 = [{ icon: '../../../assets/images/Philosophy.svg', title: 'PHILOSOPHY', nextIcon: '../../../assets/images/Next.svg', url: '' },
+    { icon: '../../../assets/images/History.svg', title: 'HISTORY', nextIcon: '../../../assets/images/Next.svg', url: '' },
+    { icon: '../../../assets/images/Adventure.svg', title: 'ADVENTURE', nextIcon: '../../../assets/images/Next.svg', url: '' }]
   }
 }
